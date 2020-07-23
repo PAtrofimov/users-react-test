0a5c20bd96ebe288e2c6c348a337f07040aefeac
Peter Trofimov <ptrofimov7@gmail.com>
2020-07-23 22:07:00 +0300

    Show Users, Months in App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Months from "./Months";
+import Users from "./Users";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [Failure, setFailure] = useState("");
+  const [users, setUsers] = useState([]);
+  
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://yalantis-react-school-api.yalantis.com/api/task0/users")
+      .then((result) => result.json())
+      .then((result) => {
+        setUsers(
+          result.map((it) => {
+            return { id: it.id, fullName: `${it.firstName} ${it.lastName}`, month: new Date(it.dob).getMonth() + 1};
+          })
+        );
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setFailure(error.message);
+      });
+  }, []);
+
+  if (isLoading) {
+    return "Loading ...";
+  }
+
+  if (Failure) {
+    return <p>Error occurs: {Failure} </p>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <header></header>
+      <Months users={users} />
+      <Users users={users} />
     </div>
   );
 }
