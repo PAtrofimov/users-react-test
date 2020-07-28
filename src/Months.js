1d4cc56250ef8d1b9ab396239017b58a002e6413
Peter Trofimov <ptrofimov7@gmail.com>
2020-07-28 22:28:35 +0300

    Added component Month

--- src/Months.js
+++ src/Months.js
@@ -1,44 +1,77 @@
 import React, { useState } from "react";
+import { MONTHS } from "./utils/constants";
 
-const Months = ({ users }) => {
-  const [usersMonth, setUsersMonth] = useState([]);
-  const handleHover = (e) => {
-    const { target } = e;
-    const newUsers = users
-      .filter((it) => it.month === +target?.innerText)
-      .sort((a, b) => {
-        if (a.fullName > b.fullName) {
-          return 1;
-        }
-        if (a.fullName < b.fullName) {
-          return -1;
-        }
-        return 0;
-      });
-    e.preventDefault();
-    setUsersMonth(newUsers);
+const Months = ({ users, monthQty, onHoverMonth }) => {
+  const [activeMonth, setActiveMonth] = useState(0);
+  const UserByName = (a, b) => {
+    if (a.fullName > b.fullName) {
+      return 1;
+    }
+    if (a.fullName < b.fullName) {
+      return -1;
+    }
+    return 0;
   };
-  return (
-    <React.Fragment>
-      {usersMonth && usersMonth.map((it) => <p> {it.fullName} </p>)}
-      <nav>
-        {Array(12)
-          .fill(0)
-          .map((val, index) => index + 1)
-          .map((it) => (
-            <a
-              href="/#"
-              key={it}
-              value={it}
-              onMouseOver={handleHover}
-              onMouseLeave={(e) => setUsersMonth([])}
-            >
-              {it}
-            </a>
-          ))}
-      </nav>
-    </React.Fragment>
-  );
+
+  const handleHover = (e) => {
+    const { number, month, qty, color } = e.target.dataset;
+    const newUsers = users
+      .filter((it) => it.month === +number)
+      .sort(UserByName);
+    setActiveMonth(number);
+    onHoverMonth({ month, qty, color, users: newUsers });
+  };
+
+  const handleLeave = (e) => {
+    setActiveMonth(0);
+    onHoverMonth({
+      month: undefined,
+      users: [],
+      color: undefined,
+      qty: 0,
+    });
+  };
+
+  const getColorMonth = (qty) => {
+    let color;
+    if (qty <= 2) {
+      color = "grey";
+    } else if (qty >= 3 && qty <= 6) {
+      color = "blue";
+    } else if (qty >= 7 && qty <= 10) {
+      color = "green";
+    } else {
+      color = "red";
+    }
+    return color;
+  };
+
+  const renderMonths = () => {
+    return MONTHS.map((it) => {
+      const qty = monthQty[it.number] ?? 0;
+      const color = getColorMonth(qty);
+      const className =
+        "month-ref" + (it.number === +activeMonth ? " active-month" : "");
+      return (
+        <a
+          href="/#"
+          key={it.number}
+          data-number={it.number}
+          data-month={it.name}
+          data-qty={qty}
+          data-color={color}
+          style={{ color }}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+          className={className}
+        >
+          <span className="month-name">{it.name}</span> ({qty})
+        </a>
+      );
+    });
+  };
+
+  return <nav className="months-wrapper">{renderMonths()}</nav>;
 };
 
 export default Months;
