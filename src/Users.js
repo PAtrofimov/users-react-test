1164bbf5c09986e3e76fa769c33e2c6a5a2dd764
Peter Trofimov <ptrofimov7@gmail.com>
2020-07-28 22:29:03 +0300

    Added Users

--- src/Users.js
+++ src/Users.js
@@ -1,19 +1,41 @@
 import React from "react";
 
-const Users = ({users}) => {
-
+const Users = ({ users, month, qty, color }) => {
   const userList =
     users.length === 0 ? (
       <p>No users</p>
     ) : (
       users.map((it) => (
         <li key={it.id}>
-          <a href="/#">{it.fullName} - {it.month} </a>
+          <a href="/#">
+            <span className="user-fullname">{it.fullName} </span>
+            <span className="user-date">({it.dob})</span>
+          </a>
         </li>
       ))
     );
 
-  return <ul>{userList}</ul>;
+  const renderUsers = () => {
+    if (!month) {
+      return null;
+    }
+    return (
+      <div>
+        <h1>
+          <span className="users-qty" style={{ color }}>
+            {qty}
+          </span>{" "}
+          Users of birthday in{" "}
+          <span className="users-month" style={{ color }}>
+            {month}
+          </span>
+        </h1>
+        <ul className="users-month-list">{userList}</ul>
+      </div>
+    );
+  };
+
+  return renderUsers();
 };
 
 export default Users;
