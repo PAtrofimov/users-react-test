73e32460c02ce9c0779b86003cf47c5a7f22d497
Peter Trofimov <ptrofimov7@gmail.com>
2020-07-29 09:53:12 +0300

    Updates

---
+++ precache-manifest.beec2ae1eecb3841c2d4c8c1f31c7eef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b17523dc08e1b1448264699296c157d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "985148f06ee3e4cd5349",
+    "url": "/static/css/main.a03e1962.chunk.css"
+  },
+  {
+    "revision": "dc42fbf442f24e1b592e",
+    "url": "/static/js/2.bc81aa87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bc81aa87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985148f06ee3e4cd5349",
+    "url": "/static/js/main.46689137.chunk.js"
+  },
+  {
+    "revision": "1f015f72d70504c71380",
+    "url": "/static/js/runtime-main.32e98e29.js"
+  }
+]);
